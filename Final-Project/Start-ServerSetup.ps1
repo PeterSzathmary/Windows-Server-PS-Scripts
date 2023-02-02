72998cd76bb8e7f5259c4b069ebbfc3f23a67080
Peter Szathmáry <pet.szathmary@gmail.com>
2023-02-02 18:08:34 +0100

    call to static ip function

--- Final-Project/Start-ServerSetup.ps1
+++ Final-Project/Start-ServerSetup.ps1
@@ -1,5 +1,6 @@
 . .\Restart-ScriptAtStartup.ps1
 . .\Disable-MapsBroker.ps1
+. .\Set-StaticIP.ps1
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'existModuleName',
         Justification = 'variable will be used later')]
@@ -14,3 +15,4 @@ $swotMembers = @()
 
 Restart-ScriptAtStartup -Path $MyInvocation.MyCommand.Path
 Disable-MapsBroker
+Set-StaticIP -StaticIP $config.staticIP

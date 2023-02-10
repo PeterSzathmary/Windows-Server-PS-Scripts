a1b904211520c9cbe1c61820a0c2059970b81df8
Peter Szathmáry <pet.szathmary@gmail.com>
2023-02-10 21:23:22 +0100

    new script for getting some outputs from sql script and saving it as log file

--- Final-Project/Get-OracleDBInfo.ps1
+++ Final-Project/Get-OracleDBInfo.ps1
@@ -3,4 +3,9 @@ $Container = "CDB`$ROOT"
 $TXTPath = "test.txt"
 $SQLInfoPath = "tablespaces_info.sql"
 
+if (!(Test-Path "C:\DB\$TXTPath")) {
+    New-Item -Path "C:\DB\$TXTPath" -Force
+}
+
+Add-Content "C:\DB\$TXTPath" "`n$Container tablespaces usage information: "
 sqlplus.exe / as sysdba "@$ScriptPath" $Container $TXTPath $SQLInfoPath
--- Final-Project/SQL/output.sql
+++ Final-Project/SQL/output.sql
@@ -3,8 +3,11 @@ set feed off
 -- &1 -> which container ?
 -- &2 -> output file path
 -- &3 -> path to sql file to execute (should be some info script)
+-- which container to use
 ALTER SESSION SET CONTAINER = &1;
-spool c:\DB\&2
+-- where to store output
+spool "c:\DB\&2" append
+-- which sql script to execute
 @c:\DB\&3
 spool off
 exit
